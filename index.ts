00b3d158a874a4f7aa8230e850655278ea8cedc2
3dnsfw <108621202+3dnsfw@users.noreply.github.com>
2024-05-23 16:04:59 +0200

    feat: parallel retryable downloads, new progress bar, chalk, prettier

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
-import fs from "fs-extra";
-import path from "path";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import { SingleBar, Presets } from "cli-progress";
+import axios, { isAxiosError } from 'axios';
+import fs from 'fs-extra';
+import path from 'path';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { MultiProgressBars } from 'multi-progress-bars';
+import chalk from 'chalk';
 
-interface Attachment {
+interface File {
   name: string;
   path: string;
 }
@@ -14,77 +15,104 @@ interface Post {
   id: string;
   user: string;
   service:
-    | "patreon"
-    | "fanbox"
-    | "discord"
-    | "fantia"
-    | "afdian"
-    | "boosty"
-    | "gumroad"
-    | "subscribestar"
-    | "onlyfans"
-    | "fansly"
-    | "candfans";
+    | 'patreon'
+    | 'fanbox'
+    | 'discord'
+    | 'fantia'
+    | 'afdian'
+    | 'boosty'
+    | 'gumroad'
+    | 'subscribestar'
+    | 'onlyfans'
+    | 'fansly'
+    | 'candfans';
   title: string;
   content: string;
   shared_file: boolean;
   added: Date;
   published: Date;
   edited: Date | null;
-  file: Attachment;
-  attachments: Attachment[];
+  file: File;
+  attachments: File[];
 }
 
-// Define command-line arguments
+interface DownloadQueueEntry {
+  url: string;
+  outputPath: string;
+  postId: string;
+}
+
 const argv = yargs(hideBin(process.argv))
-  .option("service", {
-    alias: "s",
-    type: "string",
-    description: "The service to scrape from",
+  .option('service', {
+    alias: 's',
+    type: 'string',
+    description: 'The service to scrape from',
     choices: [
-      "patreon",
-      "fanbox",
-      "discord",
-      "fantia",
-      "afdian",
-      "boosty",
-      "gumroad",
-      "subscribestar",
-      "onlyfans",
-      "fansly",
-      "candfans",
+      'patreon',
+      'fanbox',
+      'discord',
+      'fantia',
+      'afdian',
+      'boosty',
+      'gumroad',
+      'subscribestar',
+      'onlyfans',
+      'fansly',
+      'candfans',
     ],
     demandOption: true,
   })
-  .option("userId", {
-    alias: "u",
-    type: "string",
-    description: "The user ID to scrape from",
+  .option('userId', {
+    alias: 'u',
+    type: 'string',
+    description: 'The user ID to scrape from',
     demandOption: true,
   })
-  .option("host", {
-    alias: "h",
-    type: "string",
-    description: "The host to scrape from",
-    choices: ["kemono.su", "coomer.su"],
-    default: "kemono.su",
+  .option('host', {
+    alias: 'h',
+    type: 'string',
+    description: 'The host to scrape from',
+    choices: ['kemono.su', 'coomer.su'],
+    default: 'kemono.su',
   })
-  .option("cdnHost", {
-    alias: "c",
-    type: "string",
-    description: "The CDN host for downloading files",
-    choices: ["c1.kemono.su", "c6.coomer.su"],
-    default: "c1.kemono.su",
+  .option('cdnHost', {
+    alias: 'c',
+    type: 'string',
+    description: 'The CDN host for downloading files',
+    choices: ['c1.kemono.su', 'c6.coomer.su'],
+    default: 'c1.kemono.su',
+  })
+  .option('outputDir', {
+    alias: 'o',
+    type: 'string',
+    description: 'The output directory for downloads',
+    default: 'downloads-%username%',
   })
   .help()
-  .alias("help", "help").argv;
+  .alias('help', 'help').argv;
 
-const { service, userId, host, cdnHost } = argv;
+const { service, userId, host, cdnHost, outputDir } = argv;
 
 const API_URL = `https://${host}/api/v1/${service}/user/${userId}`;
 const DOWNLOAD_URL = `https://${cdnHost}/data`;
-const OUTPUT_DIR = "./downloads";
+// Replace %username% in the output directory with the actual user ID
+const OUTPUT_DIR = outputDir.replace('%username%', userId);
 const PAGE_SIZE = 50;
+const MAX_CONCURRENT_DOWNLOADS = 5;
+const MAX_DOWNLOAD_RETRIES = 3;
+const DOWNLOAD_RETRY_WAIT_SECONDS = 5000;
+
+const downloadBars = new MultiProgressBars({
+  initMessage: 'Downloads',
+  anchor: 'bottom',
+  persist: true,
+});
+
+const overallProgressBarId = 'Overall Progress';
+downloadBars.addTask(overallProgressBarId, {
+  type: 'percentage',
+  barTransformFn: chalk.yellow,
+});
 
 async function fetchPosts(offset: number = 0): Promise<Post[]> {
   const url = `${API_URL}?o=${offset}`;
@@ -92,86 +120,176 @@ async function fetchPosts(offset: number = 0): Promise<Post[]> {
   return response.data;
 }
 
-async function downloadFile(
-  url: string,
-  outputPath: string,
-  progressBar: SingleBar
-): Promise<void> {
-  const writer = fs.createWriteStream(outputPath);
-  const response = await axios.get(url, { responseType: "stream" });
-
-  response.data.pipe(writer);
-
-  return new Promise((resolve, reject) => {
-    writer.on("finish", () => {
-      progressBar.increment();
-      progressBar.update(progressBar.value, {
-        status: `Downloaded: ${outputPath}`,
+async function downloadFile(downloadQueueEntry: DownloadQueueEntry, retries = 0): Promise<void> {
+  const { url, outputPath, postId } = downloadQueueEntry;
+  try {
+    if (await fs.pathExists(outputPath)) {
+      downloadBars.done(postId, {
+        message: `File already exists ${outputPath}`,
+        barTransformFn: chalk.green,
       });
-      resolve();
+      setTimeout(() => {
+        downloadBars.removeTask(postId);
+      }, 2000);
+      return;
+    }
+
+    const response = await axios.get(url, {
+      responseType: 'stream',
+      onDownloadProgress: progressEvent => {
+        if (progressEvent.total) {
+          // can calculate percentage
+          downloadBars.updateTask(postId, {
+            percentage: progressEvent.loaded / progressEvent.total,
+            message: chalk.blue(`Downloading`),
+          });
+        } else {
+          downloadBars.updateTask(postId, {
+            percentage: progressEvent.loaded ? 100 : 0,
+            message: chalk.blue(`Downloading`),
+          });
+        }
+      },
     });
-    writer.on("error", reject);
-  });
+
+    const writer = fs.createWriteStream(outputPath);
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', () => {
+        downloadBars.done(postId, {
+          message: `Downloaded as ${outputPath}`,
+          barTransformFn: chalk.green,
+        });
+        setTimeout(() => {
+          downloadBars.removeTask(postId);
+        }, 2000);
+        resolve();
+      });
+      writer.on('error', reject);
+    });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      downloadBars.restartTask(postId, {
+        message: `Error: ${error.response?.status} - ${error.response?.statusText}. Retrying...`,
+        barTransformFn: chalk.red,
+      });
+      if (retries < MAX_DOWNLOAD_RETRIES) {
+        // Wait before retrying
+        await new Promise(resolve => setTimeout(resolve, DOWNLOAD_RETRY_WAIT_SECONDS));
+        return downloadFile(downloadQueueEntry, retries + 1);
+      } else {
+        // Exceeded max retries
+        throw error;
+      }
+    }
+  }
+}
+
+async function downloadFiles(downloadQueue: DownloadQueueEntry[]) {
+  let completedDownloads = 0;
+  const totalFiles = downloadQueue.length;
+  const activeDownloads: Promise<void>[] = [];
+
+  for (const downloadTask of downloadQueue) {
+    downloadBars.addTask(downloadTask.postId, {
+      type: 'percentage',
+      message: 'Starting...',
+      barTransformFn: chalk.blue,
+    });
+
+    if (activeDownloads.length >= MAX_CONCURRENT_DOWNLOADS) {
+      await Promise.race(activeDownloads);
+    }
+
+    const downloadPromise = downloadFile(downloadTask)
+      .then(() => {
+        const index = activeDownloads.indexOf(downloadPromise);
+        if (index !== -1) {
+          activeDownloads.splice(index, 1);
+        }
+        completedDownloads++;
+        downloadBars.updateTask(overallProgressBarId, {
+          percentage: completedDownloads / totalFiles,
+          message: `${completedDownloads}/${totalFiles} Files`,
+        });
+      })
+      .catch(error => {
+        console.error(`Failed to download ${downloadTask.url}:`, error);
+        downloadBars.updateTask(downloadTask.postId, {
+          message: `Error: ${error}`,
+          barTransformFn: chalk.red,
+        });
+      });
+
+    activeDownloads.push(downloadPromise);
+  }
+
+  await Promise.all(activeDownloads);
 }
 
 function sanitizeFileName(fileName: string): string {
-  return fileName.replace(/[\/\\\?%*:|"<>]/g, "_");
+  return fileName.replace(/[\/\\\?%*:|"<>]/g, '_');
 }
 
-async function main() {
+try {
   let offset = 0;
   let hasMorePosts = true;
-  const allPosts: Post[] = [];
+  const posts: Post[] = [];
 
   await fs.ensureDir(OUTPUT_DIR);
 
   while (hasMorePosts) {
-    const posts = await fetchPosts(offset);
-    if (posts.length === 0) {
+    const fetchedPosts = await fetchPosts(offset);
+    if (fetchedPosts.length === 0) {
       hasMorePosts = false;
       break;
     }
 
-    allPosts.push(...posts);
+    posts.push(...fetchedPosts);
     offset += PAGE_SIZE;
   }
 
-  console.log(`Loaded ${allPosts.length} posts.`);
+  console.log(chalk.yellow(`Loaded ${posts.length} posts.`));
 
-  const totalAttachments = allPosts.reduce(
-    (sum, post) => sum + post.attachments.length,
-    0
-  );
-  console.log(`Total attachments to download: ${totalAttachments}`);
+  // aggregate files
+  const files: File[] = posts.reduce((allFiles, post) => {
+    const postFiles = post.attachments.slice();
+    // add post.file if it's set and not already in there
+    if (post?.file?.path && !postFiles.some(att => att.path === post.file.path)) {
+      postFiles.push(post.file);
+    }
+    return [...allFiles, ...postFiles];
+  }, [] as File[]);
 
-  const progressBar = new SingleBar(
-    {
-      format:
-        "Progress | {bar} | {percentage}% | ETA: {eta}s | {value}/{total} Files | {status}",
-    },
-    Presets.shades_classic
-  );
-  progressBar.start(totalAttachments, 0, { status: "Starting..." });
+  const downloadQueue: DownloadQueueEntry[] = [];
 
-  for (const post of allPosts) {
-    for (const attachment of post.attachments) {
-      const sanitizedFileName = sanitizeFileName(attachment.name);
+  for (const post of posts) {
+    const postFiles: File[] = post.attachments.slice();
+    if (post.file?.path && !postFiles.some(att => att.path === post.file.path)) {
+      postFiles.push(post.file);
+    }
+
+    for (const file of postFiles) {
+      const sanitizedFileName = sanitizeFileName(file.name);
       const filePath = path.join(OUTPUT_DIR, sanitizedFileName);
-      if (await fs.pathExists(filePath)) {
-        progressBar.increment();
-        progressBar.update(progressBar.value, {
-          status: `File already exists: ${filePath}`,
-        });
-      } else {
-        const encodedName = encodeURIComponent(attachment.name);
-        const downloadUrl = `${DOWNLOAD_URL}${attachment.path}?f=${encodedName}`;
-        await downloadFile(downloadUrl, filePath, progressBar);
-      }
+      downloadQueue.push({
+        url: `${DOWNLOAD_URL}${file.path}?f=${encodeURIComponent(file.name)}`,
+        outputPath: filePath,
+        postId: post.id,
+      });
     }
   }
 
-  progressBar.update(totalAttachments, { status: "All files downloaded." });
-  progressBar.stop();
+  downloadBars.updateTask(overallProgressBarId, {
+    message: 'Starting downloads...',
+  });
+  await downloadFiles(downloadQueue);
+  downloadBars.done(overallProgressBarId, {
+    message: 'All files downloaded.',
+    barTransformFn: chalk.green,
+  });
+  downloadBars.close();
+} catch (error) {
+  console.error(chalk.red(error));
 }
-
-main().catch(console.error);
